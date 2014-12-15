3844b4b7a017ce0d2fd9037f049bb66a68fbc8c2
Chris Moser <chrismmoser@gmail.com>
2014-12-15 17:23:53 -0600

    Added movies directory

---
+++ movies/entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "http://www.youtube.com/watch?v=vwyZH85NQC4")
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", "http://www.youtube.com/watch?v=-9ceBgWV8io")
+
+print(toy_story.storyline)
+print(avatar.storyline)
