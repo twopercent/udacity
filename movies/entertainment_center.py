d5df3f689a3b5b0635def6a83c7924273e914991
Chris Moser <chrismmoser@gmail.com>
2014-12-15 17:43:07 -0600

    updated media.py

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -2,7 +2,12 @@ import media
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "http://www.youtube.com/watch?v=vwyZH85NQC4")
 
-avatar = media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", "http://www.youtube.com/watch?v=-9ceBgWV8io")
+avatar = media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", "http://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
-print(toy_story.storyline)
-print(avatar.storyline)
+the_rock = media.Movie("The Rock", "A group of rogue U.S. Force Recon Marines take control of Alcatraz", "http://upload.wikimedia.org/wikipedia/en/8/82/The_Rock_(movie).jpg", "https://www.youtube.com/watch?v=313n0wga2xo")
+
+#print(toy_story.storyline)
+#print(avatar.storyline)
+#avatar.show_trailer()
+print (the_rock.storyline)
+the_rock.show_trailer()
